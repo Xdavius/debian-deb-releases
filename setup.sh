7bfb320daaa58e884a97506758bcfab834f3a9f4
Davius <66792063+Xdavius@users.noreply.github.com>
2024-01-05 15:16:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cd /root
 
 wget https://github.com/Xdavius/debian-deb-releases/releases/download/PACKAGES/firmware-amd-graphics_20240104-1.deb
 
-echo "deb http://deb.debian.org/debian/ trixie main non-free-firmware non-free contrib" > /etc/apt/sources.list.d/sid.list
+echo "deb http://deb.debian.org/debian/ sid main non-free-firmware non-free contrib" > /etc/apt/sources.list.d/sid.list
 echo "Package : *
 Pin : release a=unstable
 Pin-Priority : -1
 " > /etc/apt/preferences.d/sid
 
 apt update -y
-apt install -y linux-image-amd64 linux-headers-amd64
+apt install -y -t sid linux-image-amd64 linux-headers-amd64
+dpkg -i firmware-amd-graphics_20240104-1.deb
